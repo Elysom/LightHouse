7cad75503ab293f301f76f7fd242f32fce69d735
TomasSilvaRoldan <tomasroldansilva@gmail.com>
2025-04-01 15:06:28 +0200

    CambiosEstilos

--- src/utiles/obtenerColor.jsx
+++ src/utiles/obtenerColor.jsx
@@ -1,5 +1,5 @@
 export function getColor(score) {
     if (score < 0.50) return "#e63946"; // Rojo
     if (score < 0.80) return "#ffb703"; // Amarillo
-    return "#2a9d8f";                   // Verde
+    return "#2adb16";                   // Verde
   }
